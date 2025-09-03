6e02ba446925fd4f1f5df73c0b62395f429687d3
Tuomas203 <tuomas.ruuskaa@gmail.com>
2025-09-03 16:59:12 +0300

    kesken

--- src/main/java/viikko1/SortThree.java
+++ src/main/java/viikko1/SortThree.java
@@ -3,6 +3,8 @@ package viikko1;
 public class SortThree {
     public static String ascending(int a, int b, int c) {
         // TODO
+        
+        
         return null;
     }
 }
--- src/main/java/viikko1/TimesTable.java
+++ src/main/java/viikko1/TimesTable.java
@@ -2,7 +2,11 @@ package viikko1;
 
 public class TimesTable {
     public static String table(int n) {
-        // TODO
-        return null;
+        //TODO
+String timestable;
+
+
+return null;
+        
     }
 }
