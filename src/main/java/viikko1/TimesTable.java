3e2a8766342e869ea083efccc1ede3d6d9fe974a
Tuomas203 <tuomas.ruuskaa@gmail.com>
2025-09-10 15:37:51 +0300

    kesken

--- src/main/java/viikko1/TimesTable.java
+++ src/main/java/viikko1/TimesTable.java
@@ -3,7 +3,11 @@ package viikko1;
 public class TimesTable {
     public static String table(int n) {
         //TODO
-String timestable;
+
+for (int i = 1; i==10; i++){
+    System.out.println(n+" x "+i+" = "+n*i);
+}
+System.out.println("/n");
 
 
 return null;
